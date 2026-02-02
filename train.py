30573334f59632fc469313e9b1d5dde0ed3246d0
Giuseppe Costantino <giuseppe.costantino95@gmail.com>
2026-02-02 17:12:47 +0100

    Create train.py

---
+++ train.py
@@ -0,0 +1,23 @@
+from config.denoiser_params import denoiser_configuration
+from dataset.h5insardataset import H5InSARDataset
+from kito import Engine
+from kito.data import GenericDataPipeline
+from model.insar_denoiser import InSARDenoiser
+
+if __name__ == '__main__':
+    denoiser = InSARDenoiser(denoiser_configuration)
+
+    engine = Engine(denoiser, denoiser_configuration)
+
+    dataset = H5InSARDataset(denoiser_configuration.data.dataset_path)
+
+    pipeline = GenericDataPipeline(
+        config=denoiser_configuration,
+        dataset=dataset,
+        preprocessing=denoiser_configuration.data.preprocessing,
+    )
+
+    pipeline.setup()
+
+    batch_idx = denoiser_configuration.model.batch_idx_viz
+    engine.fit(data_pipeline=pipeline)
