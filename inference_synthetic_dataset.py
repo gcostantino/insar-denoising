91c92ab59bdd8646e73632a56efe4bbfe3447499
Giuseppe Costantino <giuseppe.costantino95@gmail.com>
2026-02-02 17:17:10 +0100

    Create inference_synthetic_dataset.py

---
+++ inference_synthetic_dataset.py
@@ -0,0 +1,24 @@
+from config.denoiser_params import denoiser_configuration
+from dataset.h5insardataset import H5InSARDataset
+from kito import Engine
+from kito.data import GenericDataPipeline
+from model.insar_denoiser import InSARDenoiser
+
+if __name__ == '__main__':
+    denoiser = InSARDenoiser(denoiser_configuration)
+
+    engine = Engine(denoiser, denoiser_configuration)
+
+    dataset = H5InSARDataset(denoiser_configuration.data.dataset_path)
+
+    pipeline = GenericDataPipeline(
+        config=denoiser_configuration,
+        dataset=dataset,
+        preprocessing=denoiser_configuration.data.preprocessing,
+    )
+
+    pipeline.setup()
+
+    engine.module.build()
+    engine.load_weights(denoiser_configuration.model.weight_load_path)
+    engine.predict(data_pipeline=pipeline)
